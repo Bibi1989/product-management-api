bbe5b99e940fc36610f79a7e059c701c5c153d5f
bibi1989 <bibiaremieye@gmail.com>
2020-05-03 13:26:57 +0100

    added email verification

--- dist/mail/mail.js
+++ dist/mail/mail.js
@@ -6,13 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const nodemailer_1 = __importDefault(require("nodemailer"));
 const nodemailer_sendgrid = require("nodemailer-sendgrid");
 exports.sendMail = (email, message, subject, res) => {
-    let transporter = nodemailer_1.default.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_MAIL_AUTH,
-            pass: process.env.PASSWORD_MAIL_AUTH,
-        },
-    });
+    let transporter = nodemailer_1.default.createTransport(nodemailer_sendgrid({
+        apiKey: process.env.SENDGRID_API_KEY,
+    }));
+    // let transporter: any = nodemailer.createTransport({
+    //   service: "gmail",
+    //   auth: {
+    //     user: process.env.EMAIL_MAIL_AUTH,
+    //     pass: process.env.PASSWORD_MAIL_AUTH,
+    //   },
+    // });
     const mailOptions = {
         from: process.env.EMAIL_MAIL_AUTH,
         to: email,
